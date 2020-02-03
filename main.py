9647e340d11628d0f88cce5ed6556881971faaa2
hyoungseok <hyoungseok.k@gmail.com>
2020-02-03 23:21:45 +0900

    [Python] create main function

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-import time
 import openpyxl
+import collections
+import time
 from PyPDF2 import PdfFileMerger
 
-solution = {
-    "11-1": "1",
-    "11-2": "3",
-    "11-3": "-1",
-    "11-4": "delta",
-    "11-5": "42",
-}
+solution_file = openpyxl.load_workbook("data/solution.xlsx", data_only=True)
+test_file = openpyxl.load_workbook("data/test.xlsx", data_only=True)
 
-pdf_list = [
-    "data/test001.pdf",
-    "data/test002.pdf",
-    "data/test003.pdf",
-    "data/test004.pdf",
-    "data/test005.pdf",
-]
+solution_sheet = solution_file["solution"]
+test_sheet = test_file["test"]
 
-test = ["1", "2", "0", "delta", "41"]
+solution = {}
+for row in solution_sheet.iter_rows(min_row=2):
+    solution[row[0].value] = (row[1].value, row[2].value)
 
-merger = PdfFileMerger()
+test = collections.defaultdict(list)
+for row in test_sheet.iter_rows(min_row=2):
+    test[row[0].value].append([row[1].value, row[2].value])
 
-for pdf in pdf_list:
-    merger.append(pdf)
+for uid in test:
+    merger = PdfFileMerger()
+    for qid, answer in test[uid]:
+        if solution[qid][0] != answer:
+            merger.append(solution[qid][1])
 
-timestamp = time.strftime("%Y%m%d-%H%M%S", time.localtime(time.time()))
-merge_name = f"{timestamp}-000.pdf"
-merger.write(f"output/{merge_name}")
-merger.close()
-print(f"pdf exported: {merge_name}")
+    timestamp = time.strftime("%Y%m%d-%H%M%S", time.localtime(time.time()))
+    merge_name = f"{timestamp}-{uid}.pdf"
+    merger.write(f"output/{merge_name}")
+    merger.close()
+    print(f"pdf exported: {merge_name}")
